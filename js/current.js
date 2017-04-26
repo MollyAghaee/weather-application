e5c15595041dbcb201c52f5d8a5dd3ab8d7909d7
Rocco Panacci <rocco@clausehound.com>
2017-04-26 13:25:22 -0400

    first commit

---
+++ js/current.js
@@ -0,0 +1,14 @@
+// The API url and the city we want to check...
+var url = "http://api.openweathermap.org/data/2.5/weather?callback=?";
+var q = "Toronto";
+var units = "metric";
+var APPID = "6dec5fb891e6e243c9d8c20351998e67";
+
+
+// Go call the URL and pass 2 parameters (q=city, units=metric/imperial)
+$.getJSON(url, {}, function(result)
+{
+
+	console.log(data);
+
+});
