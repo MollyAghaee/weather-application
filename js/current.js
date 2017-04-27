ce3572d6c1e02d352d3296b5a265c93d52127899
MollyAghaee <mollyaghaee@Mollys-MacBook-Pro.local>
2017-04-26 21:00:28 -0400

    its working

--- js/current.js
+++ js/current.js
@@ -1,14 +1,19 @@
 // The API url and the city we want to check...
-var url = "http://api.openweathermap.org/data/2.5/weather?callback=?";
-var q = "Toronto";
-var units = "metric";
-var APPID = "6dec5fb891e6e243c9d8c20351998e67";
+var url = 'http://api.openweathermap.org/data/2.5/weather?callback=?';
+var q = 'Toronto';
+var units = 'metric';
+var APPID = '6dec5fb891e6e243c9d8c20351998e67';
 
+var send = {
+  q: 'montreal',
+  units: 'metric',
+  APPID: '6dec5fb891e6e243c9d8c20351998e67',
+};
 
 // Go call the URL and pass 2 parameters (q=city, units=metric/imperial)
-$.getJSON(url, {}, function(result)
+$.getJSON(url, send, function (result)
 {
 
-	console.log(data);
+  console.log(result);
 
 });
